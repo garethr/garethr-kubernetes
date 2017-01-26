44e3cdc07d82d189dfc476914552e9566ab18b21
Charles Teinturier <charles@daysofwonder.com>
2017-01-26 10:51:23 +0100

    Change the symbolise after swager generation

--- lib/puppet_x/puppetlabs/kubernetes/provider.rb
+++ lib/puppet_x/puppetlabs/kubernetes/provider.rb
@@ -77,7 +77,7 @@ module PuppetX
         def make_object(type, name, params)
           klass = type.split('_').collect(&:capitalize).join
           params[:metadata] = {} unless params.key?(:metadata)
-          p = params.kube_symbolize_keys.fixnumify
+          p = params.swagger_symbolize_keys.fixnumify
           object = Object::const_get("Kubeclient::#{klass}").new(p)
           object.metadata.name = name
           object.metadata.namespace = namespace unless namespace.nil?
