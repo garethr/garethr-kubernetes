5e48e55cd5db7ae870bf9042f30d3646a60be30f
Charles Teinturier <charles@daysofwonder.com>
2017-01-26 11:03:52 +0100

    Remove old symbolize_key

--- lib/puppet_x/puppetlabs/swagger/symbolize_keys.rb
+++
@@ -1,7 +0,0 @@
-class Object
-  def kube_symbolize_keys
-    return self.inject({}){|memo,(k,v)| memo[k.to_sym] = v.kube_symbolize_keys; memo} if self.is_a? Hash
-    return self.inject([]){|memo,v    | memo           << v.kube_symbolize_keys; memo} if self.is_a? Array
-    return self
-  end
-end
